0c83fb297901b3b91fcef4c55879caea73631c7f
YoussefAyman77 <youssefstemer@gmail.com>
2026-02-20 08:37:39 +0200

    first

--- task09/Program.cs
+++ task09/Program.cs
@@ -6,7 +6,7 @@ namespace task09
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Sab3!");
         }
     }
 }
